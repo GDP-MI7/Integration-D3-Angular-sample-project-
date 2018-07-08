6cee1a5837bb3c48ff179cea7e0f9c8fa54414aa
chandoorvipul <chandoorvipul@gmail.com>
2018-07-07 19:57:38 -0500

    sample integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PieChartComponent } from './06_pie_chart/pie-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule
   ],
+  entryComponents: [PieChartComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
